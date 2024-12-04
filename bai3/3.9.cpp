3434536e9c1713542322dbf1f794f4dd5e54a094
Nguyễn Trần Đức Duy <39818356+Pivemon@users.noreply.github.com>
2024-12-04 20:47:58 +0700

    Create 3.9.cpp

---
+++ bai3/3.9.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
+    double x,y,f;
+    scanf("%lf%lf",&x,&y);
+    f=pow(y,6)+x*pow(y,5)+pow(x,2)*pow(y,4)+pow(x,3)*pow(y,3)+pow(x,4)*pow(y,2)+pow(x,5)*y+pow(x,6);
+    printf("%.4lf",f);
+    return 0;
+}
